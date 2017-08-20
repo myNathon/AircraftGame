3f066abd6628eec45f4e3644cfc9f01bd80ad92e
Nathon <730382930@qq.com>
2017-08-20 19:56:02 +0800

    game main

---
+++ src/com/none/game/Main.java
@@ -0,0 +1,17 @@
+package com.none.game;
+
+import com.none.game.frame.GameFrame;
+
+/**
+ * 游戏入口
+ * Created by Nathon on 2017/8/20.
+ */
+public class Main {
+    /**
+     * start game
+     * @param args
+     */
+    public static void main(String[] args) {
+        new GameFrame().launchFrame();
+    }
+}
